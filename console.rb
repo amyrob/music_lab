53f0d738a00d8a83b192351f3d09aa60b12c6bd9
Oli <oliver.berry84@gmail.com>
2018-02-14 15:40:08 +0000

    how bout now?

--- console.rb
+++ console.rb
@@ -5,10 +5,14 @@ require_relative('models/artists')
 artist1 = Artists.new( {'name'=> 'Trivium' })
 artist1.save()
 
-
+artist2 = Artists.new ( {'name' => 'Ellie Goulding'})
+artist2.save()
 
 album1 = Albums.new( { 'title' => 'Silence In The Snow', 'genre' => 'thrash metal', 'artist_id' => artist1.id } )
 album1.save()
+
+album2 = Albums.new ( {'title' => 'Lights', 'genre' => 'electronic', 'artist_id' => artist2.id})
+album2.save()
 
 binding.pry
 
